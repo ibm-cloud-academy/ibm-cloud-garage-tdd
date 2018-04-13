0977b55c2f75afad41292f06fb2b41b0ca576985
Victor Leung <vicleung@au1.ibm.com>
2018-04-13 11:04:51 +0200

    update test description

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -11,8 +11,8 @@ describe('a stack', () => {
   it('stack size is 1 when pushed');
   it('stack is empty when pushed and popped');
   it('stack size is 0 when pushed and popped');
-  it('overflows');
-  it('underflows');
+  it('throws overflow error when pushing to a stack at full capacity');
+  it('throw underflow error when popping an empty stack');
   it('pops the same one item when pushed');
   it('pops two items with the most recent first');
   it('accepts only a positive capacity');
