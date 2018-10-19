fa60dd179cc5a332cb2289b3793060d009a3e256
Dominic Harries <pair+dharries@garage>
2018-10-19 16:10:46 +0200

    Reject promise properly

--- server/async-02-line-count-with-promise/line-count.js
+++ server/async-02-line-count-with-promise/line-count.js
@@ -5,7 +5,8 @@ const readFile = promisify(fs.readFile);
 
 const lineCount = filename => {
   const onSuccess = data => data.split("\n").length;
-  const onError = () => new Error("problem reading file: " + filename);
+  const onError = () =>
+    Promise.reject(new Error("problem reading file: " + filename));
 
   return readFile(filename, "UTF-8")
     .then(onSuccess)
